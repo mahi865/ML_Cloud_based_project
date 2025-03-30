f486487b4918c36a1fb7b52402a7066d89601c42
mahidhar <mahidharavula3@gmail.com>
2025-03-30 11:32:49 -0400

    added additional code changes in data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -17,6 +18,7 @@ class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
+
 
 class DataIngestion:
     def __init__(self):
